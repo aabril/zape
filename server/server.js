250d48da173aa2847e89481f43354aef87e07810
aabril <albert.abril@gmail.com>
2016-03-02 01:01:45 +0000

    Using ES6 for server call

--- server/server.js
+++ server/server.js
@@ -7,7 +7,7 @@ app.server = http.createServer(app);
 
 app.use('/', (req, res) => res.send("hello world"));
 
-app.server.listen(env.port, env.hostname, function () {
+app.server.listen(env.port, env.hostname, () => {
   console.log('Express server listening on %d, in %s mode', env.port, app.get('env'));
 });
 
