63f4456d9f4e657999892fd838abb554059497a5
aabril <albert.abril@gmail.com>
2016-03-02 01:50:06 +0000

    Adding Bunyan as logger to output in JSON format (and get it ready to handle different loggers and log levels and external servers as loggly)

--- server/server.js
+++ server/server.js
@@ -1,14 +1,17 @@
 import http from 'http';
 import express from 'express';
 import env from './config/environments/dev.json'
+import bunyan from 'bunyan'
 
 let app = express();
 app.server = http.createServer(app);
 
+let log = bunyan.createLogger({name: 'play', level: 'info'});
+
 app.use('/', (req, res) => res.send("hello world"));
 
 app.server.listen(env.port, env.hostname, () => {
-  console.log('Express server listening on %d, in %s mode', env.port, app.get('env'));
+  log.info('Express server listening on %d, in %s mode', env.port, app.get('env'));
 });
 
 export default app;
