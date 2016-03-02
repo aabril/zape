84504783dfd03b996fd4e323c12da1f9be5172cc
aabril <albert.abril@gmail.com>
2016-03-02 01:00:50 +0000

    Adding config/environments json

--- server/server.js
+++ server/server.js
@@ -1,13 +1,14 @@
 import http from 'http';
 import express from 'express';
+import env from './config/environments/dev.json'
 
 var app = express();
 app.server = http.createServer(app);
 
 app.use('/', (req, res) => res.send("hello world"));
 
-app.server.listen(8080, "127.0.0.1", function () {
-  console.log('Express server listening on %d, in %s mode', 8080, app.get('env'));
+app.server.listen(env.port, env.hostname, function () {
+  console.log('Express server listening on %d, in %s mode', env.port, app.get('env'));
 });
 
 export default app;
