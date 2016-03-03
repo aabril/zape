d03d5002da01e8f54167f96d7f49317b62260750
aabril <albert.abril@gmail.com>
2016-03-03 19:17:58 +0000

    Adding passport to set Authentication. (will be driven by JWT)

--- server/auth/index.js
+++ server/auth/index.js
@@ -1,5 +1,16 @@
+import express from 'express'
 import router from 'express'
+import passport from 'passport'
+import config from '../config/environments/dev.json'
+
+import local from './local'
+
 const Router = router();
+
+Router.use('/local', local);
+// router.use('/twitter', twitter);
+// router.use('/google', google);
+// router.use('/facebook', facebook);
 
 Router.get('*', function(req, res){
   res.status(404).json({"module":"auth", "msg":"endpoint not found", "status":404});
