569eb927809f43273902f930e42f0450b00c0504
Albert Abril <albert.abril@gmail.com>
2017-07-01 01:50:45 +0200

    create a temporally milou-test-database with pid appended, and destroy it after all tests passed

--- test/index.js
+++ test/index.js
@@ -3,6 +3,7 @@ import config from '../config.json'
 process.env.NODE_ENV = 'test'
 process.env.PORT = process.env.TEST_PORT || config.TEST_PORT
 process.env.MONGO_URI = process.env.TEST_MONGO_URI || config.TEST_MONGO_URI
+process.env.MONGO_URI = process.env.MONGO_URI + '-' + process.pid
 
 import mongoose from 'mongoose'
 const server = require('../server/server').default
@@ -15,10 +16,6 @@ import usersSpec from '../server/resources/user/user.spec'
 
 mongoose.Promise = Promise;
 
-test.before(async t => {
-  await mongoose.connection.dropDatabase(error => { if(error) console.log(error);});
-})
-
 test('root:get', async t => {
   t.plan(1);
   const res = await supertest(server).get('/')
@@ -30,5 +27,5 @@ usersSpec(test, server, supertest)
 thingsSpec(test, server, supertest)
 
 test.after.always('guaranteed cleanup', async t => {
-  mongoose.disconnect()
+  await mongoose.connection.dropDatabase(error => { if(error) console.log(error);});
 });
